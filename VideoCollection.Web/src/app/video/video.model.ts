aca89f6b3cc00de39d75b8489040323c608199a9
Quinntyne Brown <quinntynebrown@gmail.com>
2017-01-30 18:00:47 -0500

    video model

--- VideoCollection.Web/src/app/video/video.model.ts
+++ VideoCollection.Web/src/app/video/video.model.ts
@@ -1,4 +1,14 @@
 export class Video { 
-	public id:any;
-	public name:string;
+    public category: string;
+    public title: string;
+    public slug: string;
+    public youTubeVideoId: string;
+    public abstract: string;
+    public description: string;
+    public publishedOn: Date;
+    public publishedBy: string;
+    public createdOn: Date;
+    public createdBy: string;
+    public lastModifiedOn: Date;
+    public lastModifiedBy: string;
 }
