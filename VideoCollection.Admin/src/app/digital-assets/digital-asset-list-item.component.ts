57cd158513ac684641632128b8bfa7981e686a72
Quinntyne Brown <quinntynebrown@gmail.com>
2017-02-07 07:09:55 -0500

    digital-assets

--- VideoCollection.Admin/src/app/digital-assets/digital-asset-list-item.component.ts
+++ VideoCollection.Admin/src/app/digital-assets/digital-asset-list-item.component.ts
@@ -1,8 +1,38 @@
-import { Component, ChangeDetectionStrategy, Input, OnInit } from "@angular/core";
+import { Component, ChangeDetectionStrategy, Input, OnInit, EventEmitter, Output } from "@angular/core";
+import { DigitalAsset } from "./digital-asset.model";
+import { DigitalAssetService } from "./digital-asset.service";
+import { Router } from "@angular/router";
 
 @Component({
     template: require("./digital-asset-list-item.component.html"),
     styles: [require("./digital-asset-list-item.component.scss")],
     selector: "digital-asset-list-item"
 })
-export class DigitalAssetListItemComponent {  }
+export class DigitalAssetListItemComponent {
+    constructor(
+        private _digitalAssetService: DigitalAssetService,
+        private _router: Router
+    ) {
+
+    }
+    
+    @Input()
+    public set digitalAsset(value: DigitalAsset) { this._digitalAsset = value; }
+
+    public get digitalAsset():DigitalAsset { return this._digitalAsset; }
+
+    private _digitalAsset: DigitalAsset = <DigitalAsset>{};
+
+    public onEdit() {
+        this._router.navigate(["digitalasset","edit",this.digitalAsset.id])
+    }
+
+    @Output()
+    onDeleted = new EventEmitter();
+
+    public onDelete() {
+        this.onDeleted.emit({
+            digitalAssetId: this.digitalAsset.id
+        });
+    }
+}
